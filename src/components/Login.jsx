42958634fa7bd9dba5f4c55418b4ac82f82121d0
Yash Pal <pyash3137@gmail.com>
2025-05-10 18:23:51 +0530

    done for the day added redux config added cors in backend and few fixes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const Login = () => {
+  const [emailId,setEmailId]=useState("");
+  const [password,setPassword]=useState("");
+  const dispatch = useDispatch();
+  const navigate =useNavigate();
+  const handleLogin = async()=>{
+      try {
+         const res = await  axios.post("http://localhost:3000/login",{
+          emailId,
+          password
+        },{
+          withCredentials:true
+        })
+        dispatch(addUser(res.data));
+        navigate("/");
+        
+      } catch (error) {
+        console.error(error)
+      }
+  }
+
+
+
   return (
-    <div>Login</div>
+    <div className='flex justify-center my-12'>
+    <div className="card bg-base-300 w-96 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title justify-center">Login</h2>
+   <div>
+    <label className="form-control w-full max-w-xs my-2">
+  <div className="label py-4">
+    <span className="label-text">Email Id</span>
+  </div>
+  <input type="text" placeholder="Type here" value={emailId} onChange={(e)=>{setEmailId(e.target.value)}} className="input input-bordered w-full max-w-xs" />
+</label>
+ <label className="form-control w-full max-w-xs my-2">
+  <div className="label py-4">
+    <span className="label-text">Password</span>
+  </div>
+  <input type="text" placeholder="Type here" value={password} onChange={(e)=>{setPassword(e.target.value)}} className="input input-bordered w-full max-w-xs" />
+</label>
+
+   </div>
+    <div className="card-actions justify-center">
+      <button onClick={handleLogin} className="btn btn-primary">Login</button>
+    </div>
+  </div>
+</div>
+</div>
   )
 }
 
-export default Login
+export default Login  
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,34 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+  const user = useSelector(store=>store.user)
+ 
   return (
-   <div className="navbar bg-base-300 shadow-sm">
+   <div className="navbar bg-base-300">
   <div className="flex-1">
     <a className="btn btn-ghost text-xl">devConnect</a>
   </div>
-  <div className="flex gap-2">
-   
-    <div className="dropdown dropdown-end mx-5">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
+  <div className="flex-none gap-2">
+    
+    <div className="dropdown dropdown-end flex ">
+      {user&& (  <p className=" px-4 text-sm font-medium mr-4">Welcome, {user.firstName}</p>)}
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mx-4">
+        
+        
+       { user && (<div className="flex  gap-2 pr-2">
+          
+            <div className="w-10 rounded-full overflow-hidden">
+              <img
+                alt="user"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>)}
       </div>
       <ul
         tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
         <li>
           <a className="justify-between">
             Profile
