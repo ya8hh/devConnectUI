ad9b8a56eb3fdb351b2fc0ab591c51518ffa8273
Yash Pal <pyash3137@gmail.com>
2025-05-11 10:05:22 +0530

    feature

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import axios from "axios"
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser} from "../utils/userSlice"
 
 const Body = () => {
+  const dispatch =useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store)=>store.user);
+
+  const fetchUser = async()=>{
+    try {
+        const res = await axios.get("http://localhost:3000/profile/view",
+      {
+        withCredentials:true,
+      }
+    )
+    dispatch(addUser(res.data))
+    } catch (error) {
+      if(error.status===401)
+     { navigate("/login");}
+      console.error(error);
+      
+    }
+  }
+  useEffect(()=>{
+    if(!userData){
+      fetchUser();
+    }
+  }, []);
   return (
    <>
     <Navbar/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   const user = useSelector(store=>store.user)
@@ -7,7 +8,7 @@ const Navbar = () => {
   return (
    <div className="navbar bg-base-300">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">devConnect</a>
+    <Link to="/" className="btn btn-ghost text-xl">devConnect</Link>
   </div>
   <div className="flex-none gap-2">
     
@@ -30,10 +31,10 @@ const Navbar = () => {
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
         <li>
-          <a className="justify-between">
+          <Link to="/profile" className="justify-between">
             Profile
             <span className="badge">New</span>
-          </a>
+          </Link>
         </li>
         <li><a>Settings</a></li>
         <li><a>Logout</a></li>
